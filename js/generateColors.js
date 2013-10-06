1b65e2882b28c5488268ef4d0d0406a9cf7b13de
Trang Dang <trangd@mit.edu>
2013-10-05 23:54:08 -0400

    finally fixed ajax problem

--- js/generateColors.js
+++ js/generateColors.js
@@ -1,26 +1,34 @@
 function getPalettesFromFirstInput(hexColor) {
-    
-    var secondaryColors = new Array();
-    var url = "http://www.colourlovers.com/api/palettes/top?jsonCallback=?&format=json&hex=" + hexColor;
-
-    $.ajax({
+    var url = "http://www.colourlovers.com/api/palettes/top?jsonCallback=?&format=json&hex=";
+    return $.ajax({
         // contentType: "application/json",
         dataType: 'jsonp',
         type: "GET",
-        url: url,
+        url: url + hexColor,
         success: function(data) {
-            $.each(data[0].colors, function(ind, item){
-                secondaryColors.push(item);
-            });
-            // sketchy code ahead
-            var i = 0;
-            while (secondaryColors.length < 8) {
-                secondaryColors.push(data[1].colors[i]);
-                i = i+1;
-            }
+            processData(data);
         }
     });
 }
+
+function processData(data) {
+    var secondaryColors = new Array();
+
+    $.each(data[0].colors, function(ind, item){
+        secondaryColors.push(item);
+    });
+    // sketchy code ahead
+    var i = 0;
+    while (secondaryColors.length < 8) {
+        secondaryColors.push(data[1].colors[i]);
+        i = i+1;
+    }        
+
+    $.each(secondaryColors, function(i, item){
+        $('body').append(item);
+    });
+}
+
 
 function getPalettesFromFirstInputForRandomAlg(hexColor) {
     
